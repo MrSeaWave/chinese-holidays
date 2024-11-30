6a11a1716c88cdc97d1aac988d8fd28f90054a21
MrSeaWave <MrDaemon@outlook.com>
2024-11-30 16:19:45 +0800

    feat: modify build config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -21,24 +21,38 @@ const banner = `
 const config = [
   {
     input: 'src/index.ts',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        banner,
-        exports: 'auto',
-        // sourcemap: true,
-      },
-      {
-        file: pkg.module,
-        format: 'esm',
-        banner,
-        // sourcemap: true,
-      },
-    ],
-    external: [...dependencies],
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      banner,
+      exports: 'auto',
+      // sourcemap: true,
+    },
     // Rollup will only exclude modules that match strings exactly.
-    plugins: [commonjs(), nodeResolve(), typescript()],
+    external: [...dependencies],
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      // 代码中使用的最新的语法会被转译成ES6
+      typescript({ target: 'ES6', module: 'ES6' }),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: pkg.module,
+      format: 'esm',
+      banner,
+      // sourcemap: true,
+    },
+    // Rollup will only exclude modules that match strings exactly.
+    external: [...dependencies],
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      // 使用tsconfig定义的语法
+      typescript(),
+    ],
   },
   {
     // 生成 .d.ts 类型声明文件
--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,7 @@ export const isHoliday = holidays.isHoliday;
 export const isWeekEnd = holidays.isWeekEnd;
 export const getDateInfo = holidays.getDateInfo;
 export const getHolidaysCache = holidays.getHolidaysCache;
+// 测试编译包的语法，不要使用
+export const __isHoliday__ = holidays?.isHoliday;
 
 export { Holidays };
