243beeeb55bd3865844c8f01101a3730ebdae105
MrSeaWave <MrDaemon@outlook.com>
2024-12-01 20:53:17 +0800

    docs: update

--- src/core/request.ts
+++ src/core/request.ts
@@ -1,24 +1,24 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-import { BASE_URL } from './config';
+// import { BASE_URL } from './config';
 
-const request = (url: string, data: any, method = 'get') => {
-  const requestUrl = BASE_URL + url;
-  const options: any = {
-    method,
-    url: requestUrl,
-  };
-  if (method !== 'get') {
-    options.data = data;
-  } else {
-    options.params = data;
-  }
-  return axios(options).then((resp) => ({ success: true, data: resp.data }));
-};
+// const request = (url: string, data: any, method = 'get') => {
+//   const requestUrl = BASE_URL + url;
+//   const options: any = {
+//     method,
+//     url: requestUrl,
+//   };
+//   if (method !== 'get') {
+//     options.data = data;
+//   } else {
+//     options.params = data;
+//   }
+//   return axios(options).then((resp) => ({ success: true, data: resp.data }));
+// };
 
-/**
- * @deprecated 不再使用
- * @param param0
- * @returns
- */
-export const getMethod = ({ url, data }: { url: string; data?: any }) => request(url, data, 'get');
+// /**
+//  * @deprecated 不再使用
+//  * @param param0
+//  * @returns
+//  */
+// export const getMethod = ({ url, data }: { url: string; data?: any }) => request(url, data, 'get');
